6d32396c94a365eb278e4a1ac0f5bc92e77425c5
danirosehill <danirosehill@192.168.0.172>
2020-10-28 15:32:22 -0700

    fvfdgfd

--- src/_05_for_loops/_2_badgers/module-info.java
+++ src/_05_for_loops/_2_badgers/module-info.java
@@ -1,2 +1,13 @@
-module badgers {
+public static void main(String[] args) {
+
+int numberb= 0;
+int numberm= 0;
+
+for (numberb = 0; numberb <= 12; numberb++) {
+      System.out.println("badger");
 }
+
+for (numberm= 0; numberm<=2; number++) {
+          System.out.println("mushroom");
+}
+
