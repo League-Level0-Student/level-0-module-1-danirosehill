95df27dafa335a097eaa8f999eb11cf1436f3b3a
danirosehill <danirosehill@192.168.0.172>
2020-10-29 17:23:02 -0700

    cvcvc

---
+++ src/_05_for_loops/_2_badgers/badger.java
@@ -0,0 +1,29 @@
+package _05_for_loops._2_badgers;
+
+public class badger {
+
+	public static void main(String[] args) {
+		
+		
+
+			int numberb= 0;
+			int numberm= 0;
+			int numberverse=0;
+			
+			for(numberverse=0; numberverse < 2; numberverse++) {
+
+			for (numberb = 0; numberb < 12; numberb++) {
+			      System.out.print("badger ");
+			}
+
+			for (numberm= 0; numberm<2; numberm++) {
+			          System.out.print("mushroom " );
+			          
+			          System.out.println();
+			}
+
+			}
+			System.out.print("A snake!");
+	}
+	
+}
